ff4298d649295420185c34ed5c1e38fa50603596
yashvigarg <garg.yas@northeastern.edu>
2023-11-28 00:59:49 -0800

    added my dao classes

---
+++ 03_JDBC/Milestone3Y/src/main/java/game/model/GearJob.java
@@ -0,0 +1,16 @@
+public class GearJob {
+   private Item item;
+    private Job job;
+	public Item getItem() {
+		return item;
+	}
+	public void setItem(Item item) {
+		this.item = item;
+	}
+	public Job getJob() {
+		return job;
+	}
+	public void setJob(Job job) {
+		this.job = job;
+	}
+}
